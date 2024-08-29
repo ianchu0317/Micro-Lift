b70620b3b91697b9daa25ca73625eca82cc7f5b6
Ian Chen <chenian317@gmail.com>
2024-08-29 10:52:12 -0300

    Create controller.py

---
+++ src/controller.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+# button config
+
+# leds config
+
+# global setting
+ 
+
+
+
+
+print("Cleaning GPIO states...")
+GPIO.cleanup()
