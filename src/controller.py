330b3cc17e7251482fbaab91ed180ee13b941604
Ian Chen <chenian317@gmail.com>
2024-09-03 12:31:08 -0300

    actualizar script con algoritmo funcional ascensor

--- src/controller.py
+++ src/controller.py
@@ -6,11 +6,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 
-# Función que manipula entrada de un boton 
+# Función que manipula entrada de un boton
 def al_presionar(pin_boton):
 	global en_espera
 	global en_movimiento
+
 	piso = BOTONES.index(pin_boton)
+	
+	# añadir piso a espera si no está 
 	if piso not in en_espera:
 		en_espera.append(piso)
 		en_movimiento = True
@@ -38,7 +41,18 @@ def chequear_direccion(piso_dest):
 		direccion_actual = -1  # Dirección abajo
 
 
-# Función de mover al piso por número de index
+# Función de actualizar variables globales
+def actualizar_variables_por_piso(piso):
+	global piso_actual
+	global pin_led_amarillo_actual
+	global pin_led_rojo_actual 
+
+	piso_actual = piso 
+	pin_led_amarillo_actual = LEDS_AMARILLOS[piso_actual]
+	pin_led_rojo_actual = LEDS_ROJOS[piso_actual]
+
+
+# Función de mover al piso por nº de index
 def mover_a(piso):
 	global piso_actual
 	global en_espera
@@ -46,40 +60,43 @@ def mover_a(piso):
 
 	# Mover a cada piso
 	for p in range(piso_actual, piso + direccion_actual, direccion_actual):
-		prender_led(LEDS_AMARILLOS[p])
-		sleep(tiempo_en_movimiento)
-		apagar_led(LEDS_AMARILLOS[p])
-		piso_actual = p
-		print(f"Actualmente en piso {piso_actual}")
+		actualizar_variables_por_piso(p)
+
+		#print(f"Actualmente en piso {piso_actual}")
+		#print(f"Lista de espera es: {en_espera}")
+		prender_led(pin_led_amarillo_actual)
+		
+		# Chequear pisos intermedios si está en espera
+		if p in en_espera and p != piso:
+			prender_led(pin_led_rojo_actual)
+			sleep(tiempo_en_espera)
+			apagar_led(pin_led_rojo_actual)
+			en_espera.remove(piso_actual)
+
+		# cambio de piso
+		sleep(tiempo_en_movimiento)  # Movimiento a piso siguiente
+		apagar_led(pin_led_amarillo_actual)
 
 	en_espera.remove(piso)
-	en_movimiento = False
 
-
-# Chequear el estado de los botones
-def chequear_estado():
-	if en_movimiento:
-		mover_a()
-		# Apagar la luz roja del piso actual
-		apagar_led(LEDS_ROJOS[piso_actual])
-	else:
-		# Prender luz roja
-		prender_led(LEDS_ROJOS[piso_actual])
+	if len(en_espera) == 0:
+		en_movimiento = False  # Detener movimiento si no hay más pisos que ir
 
 
 def loop():
 	try:
 		while True:
 			if en_movimiento:
+				apagar_led(pin_led_rojo_actual)
 				# Mover al piso solicitado
-				apagar_led(LEDS_ROJOS[piso_actual])
 				for p in en_espera:
 					chequear_direccion(p)
 					mover_a(p)
 			else:
 				# Detener el movimiento
-				# Prender luz roja
-				prender_led(LEDS_ROJOS[piso_actual])
+				# Prender luz roja y amarilla
+				prender_led(pin_led_rojo_actual)
+				prender_led(pin_led_amarillo_actual)
 			sleep(0.1)
 	except KeyboardInterrupt:
 		print("Saliendo del bucle!")
@@ -97,16 +114,22 @@ GPIO.setup(FUENTES, GPIO.OUT, initial=GPIO.HIGH)
 GPIO.setup(BOTONES, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 for b in BOTONES:
 	GPIO.add_event_detect(b, GPIO.RISING, callback=al_presionar, bouncetime=300)
-en_espera = []  # Valores numéricos de N-piso, index
+
+# Variables del programa
+en_espera = []  # Valores numéricos de nº index
 en_movimiento = False  # Inicio detenido
+tiempo_en_movimiento = 2 # 2s de movimiento entre cada piso
+tiempo_en_espera = 3  # 3s de espera de entrepiso
 direccion_actual = 1   # 1: arriba, -1: abajo
-piso_actual = 0  
-tiempo_en_movimiento = 3 # 3s Entre cada piso
+piso_actual = 0
+pin_led_amarillo_actual = LEDS_AMARILLOS[piso_actual]
+pin_led_rojo_actual = LEDS_ROJOS[piso_actual]  
 
 
 # Ejecución
 loop()
 input("...")
 
+# Salida y cierre del programa
 print("Saliendo del programa...")
 GPIO.cleanup()
